bc5f34475d74888648413d53b2a1fce95bb7c5ce
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-04-18 16:51:35 -0300

    feat: add animation when removing patients

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,14 @@
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode; //TR = paciente = remover
-    paiDoAlvo.remove();
-})
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+});
+    
+    //var alvoEvento = event.target;
+    //var paiDoAlvo = alvoEvento.parentNode; //TR = paciente = remover
+    //paiDoAlvo.remove();
 
