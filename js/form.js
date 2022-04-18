f68ecbb6bac1c3b5854731d2eaca5efb19e67d23
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-04-18 15:45:06 -0300

    feat: add validation of the other fields

--- js/form.js
+++ js/form.js
@@ -8,21 +8,35 @@ botaoAdicionar.addEventListener("click",function(event){
 
     var pacienteTr = montaTr(paciente);
 
-    var erro = validaPaciente(paciente);
-
+    var erros = validaPaciente(paciente);
+    console.log(erros);
     if(erros.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+        exibeMensagensDeErro(erros);
         return;
     }
    
+    //adicionando pacientes na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
 
 function obtemPacienteDoFormulario(form){
 
@@ -60,6 +74,10 @@ function montaTd(dado,classe){
 function validaPaciente(paciente){
    
     var erros = [];
+
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
    
     if(!validaPeso(paciente.peso)){
         erros.push("O peso é inválido"); 
@@ -67,6 +85,18 @@ function validaPaciente(paciente){
 
     if(!validaAltura(paciente.altura)){
         erros.push("A altura é inválida");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco");
     }
 
     return erros;
