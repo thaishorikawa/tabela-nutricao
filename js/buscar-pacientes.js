9c13f07baf7c44b2f11966a03ab9e591c2a2f03e
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-04-20 17:14:55 -0300

    feat: add capturing errors

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -8,17 +8,23 @@ botaoAdicionar.addEventListener("click", function () {
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function () {
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
+        var erroAjax = document.querySelector("#erro-ajax");
 
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        }); 
-        
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
 
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+
+            erroAjax.classList.remove("invisivel");
+        }
     });
-
     xhr.send();
-
-
 })
