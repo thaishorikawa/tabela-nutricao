c18b842d048cc34a18dd9c8b23e244f7be249263
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-04-19 22:42:32 -0300

    feat: add AJAX search of patients

--- js/form.js
+++ js/form.js
@@ -5,26 +5,27 @@ botaoAdicionar.addEventListener("click", function (event) {
 
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
-
-    var pacienteTr = montaTr(paciente);
-
+    
     var erros = validaPaciente(paciente);
     console.log(erros);
     if (erros.length > 0) {
         exibeMensagensDeErro(erros);
         return;
     }
-
-    //adicionando pacientes na tabela
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+  
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros) {
     var ul = document.querySelector("#mensagens-erro");
